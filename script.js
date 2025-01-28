a442e1bb44c66931a5980744fa62714f4333ab7d
SOUGATA SAHA <sahasougata820@gmail.com>
2025-01-29 02:58:07 +0530

    add difficultly level

--- script.js
+++ script.js
@@ -2,25 +2,56 @@ function random(){
     return Math.floor(Math.random()*100+1);
 }
 let random_number;
+let guess_number=5;
 const startbtn=document.querySelector('.start');
+const level=document.querySelectorAll('.difficulty button');
 
 function removeStart(){
     document.querySelector('.start').style.display='none';
+
   
 }
 function removeAgain(){
+        guess_number=6;
     document.querySelector('.again').style.display='none';
     document.querySelector('.remarks').innerHTML='No remarks';
     document.querySelector('.inputbox input').value='';
     document.querySelector('.inputbox input').placeholder='Enter your guess';
+
 }
 
 startbtn.addEventListener('click',function(e){
-    document.querySelector('.inputbox').style.display='block';
-    document.querySelector('.gamebox h3').style.display='block';
+
+    level[0].style.display='block';
+    level[1].style.display='block';
+    level[2].style.display='block';
     
     removeStart();
     random_number=random();
+});
+level[0].addEventListener('click',function(e){
+    random_number=random();
+    level[0].style.display='none';
+    level[1].style.display='none';
+    level[2].style.display='none';
+    document.querySelector('.inputbox').style.display='block';
+    document.querySelector('.gamebox h3').style.display='block';
+});
+level[1].addEventListener('click',function(e){
+    random_number=random();
+    level[0].style.display='none';
+    level[1].style.display='none';
+    level[2].style.display='none';
+    document.querySelector('.inputbox').style.display='block';
+    document.querySelector('.gamebox h3').style.display='block';
+});
+level[2].addEventListener('click',function(e){
+    random_number=random();
+    level[0].style.display='none';
+    level[1].style.display='none';
+    level[2].style.display='none';
+    document.querySelector('.inputbox').style.display='block';
+    document.querySelector('.gamebox h3').style.display='block';
 });
 document.querySelector('.again').addEventListener('click',function(e){
     random_number=random();
@@ -53,6 +84,12 @@ function cheakGuess(gueeed_number){
 
 submitbtn.addEventListener('click',function(e){
     console.log(random_number);
+    if(guess_number===0){
+        remaks.innerHTML='You lost the game';
+        get_input.value=random_number;
+        document.querySelector('.again').style.display='block';
+        return;
+    }
     
     const remark=cheakGuess(get_input.value);
     get_input.value='';
@@ -64,7 +101,7 @@ submitbtn.addEventListener('click',function(e){
         get_input.value=random_number;
         document.querySelector('.again').style.display='block';
     }
-
+    guess_number--;
     
 })
 
